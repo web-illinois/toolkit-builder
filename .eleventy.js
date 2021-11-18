594705a24c88a9be31198f4ca2b3ef1ce1c9b030
Bryan Jonker <jonker@illinois.edu>
2021-11-18 12:52:14 -0600

    Updates for UI review

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,22 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats(['html', 'md', 'njk', '11ty.js', 'css', 'jpg', 'json']);
 
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  var markdownIt = require("markdown-it")(options);
+  var markdownItAttrs = require('markdown-it-attrs');
+
+  markdownIt.use(markdownItAttrs, {
+    leftDelimiter: '{',
+    rightDelimiter: '}',
+    allowedAttributes: []
+  });
+
+  eleventyConfig.setLibrary("md", markdownIt);
+
   return {
     dir: {
       input: "site"
---
+++ README.md
@@ -0,0 +1,13 @@
+# Web toolkit builder
+
+The goal of this is to help test the web components and to serve as a training module for new users.
+
+You can run the following commands to build the test site:
+
+````
+npm install
+npm rebuild
+npm run build-html
+````
+
+This will generate a static site under _site. You can use a live server tool to run this from your local machine. 
