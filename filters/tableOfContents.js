d5a3465d87fe540c63ad2af7786a7fa5e76db7cf
Bryan Jonker <jonker@illinois.edu>
2022-02-04 06:25:53 -0600

    Rearranging table of contents

--- filters/tableOfContents.js
+++ filters/tableOfContents.js
@@ -4,17 +4,18 @@ module.exports = class TableOfContents {
         let returnValue = "<h3>Web Component List for " + environmenttype + "</h3><table><caption class='il-invisible'>Component Information</caption>";
         returnValue += "<tr><th scope='col'>Name</th><th scope='col'>Documentation</th>";
 
+        if (environmenttype == "production") {
+            returnValue += "<th scope='col'>Preview</th>";
+        }
+
         environments.forEach(e => {
             if (environmenttype == e.type) {
                 returnValue += "<th scope='col'>" + e.name + "</th>";
             }
         });
-        if (environmenttype == "production") {
-            returnValue += "<th scope='col'>Preview</th>";
-        }
 
         returnValue += `<tr><th scope='row'>Setup Information</th>`;
-        returnValue += `<td><a href='https://github.com/web-illinois/toolkit/wiki/Recommended-HTML-Header'>Read Documentation</a></td>`;
+        returnValue += `<td><a href='https://github.com/web-illinois/toolkit/wiki/Recommended-HTML-Header'>Read Documentation</a></td><td> -- </td>`;
         environments.forEach(e => {
             if (environmenttype == e.type) {
                 returnValue += `<td><a href='/${e.slug}/index.html'>Get Header</a></td>`;
@@ -26,19 +27,22 @@ module.exports = class TableOfContents {
             if (c.environment == null || c.environment == '' || c.environment == environmenttype) {
                 returnValue += `<tr><th scope='row'>${c.title}</th>`;
                 returnValue += `<td><a href='${c.documentation}'>Read Documentation</a></td>`;
+                if (environmenttype == "production") {
+                    if (c.preview != null && c.preview) {
+                        returnValue += `<td><a href='/preview/${c.slug}/index.html'>Preview</a></td>`;
+                    } else {
+                        returnValue += '<td> -- </td>';
+                    }
+                }
                 environments.forEach(e => {
                     if (environmenttype == e.type) {
                         returnValue += `<td><a href='/${e.slug}/${c.slug}/index.html'>Build</a></td>`;
                     }
                 });
-                if (environmenttype == "production" && c.preview != null && c.preview) {
-                    returnValue += `<td><a href='/preview/${c.slug}/index.html'>Preview</a></td>`;
-                }
                 returnValue += "</tr>";
             }
         });
         returnValue += "</table>";
         return returnValue;
     }
-
 }
